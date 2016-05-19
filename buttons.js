07f63aba4020cc44da0d5635718d27e5eb5988cd
Nathan Vonnahme <nathan@careevolution.com>
2016-05-18 23:53:05 -0400

    Changes for 2016 layout

--- buttons.js
+++ buttons.js
@@ -13,18 +13,19 @@ var maxcols = 2;
 // VERY IMPORTANT: design should fit in 2.25 in diameter but bleed/margin to 2.75 diameter
 var circle_radius = 2.75/2;
 var design_radius = 2.25/2;
-var circle_margin = .18;
+var circle_margin = .28;
 var bleed_factor = 1.02;
 var img_radius = circle_radius * bleed_factor;
 
-
-var name_yoffset = .25;
+// THIS IS THE FIRST THING TO ADJUST
+// to move the name up (smaller/negative) or down
+var name_yoffset = .025;
 
 var ppi = 72;
 
 function drawButtonPDF () {
     doc = new jsPDF('portrait', 'in', 'letter');
-    
+
 
     var names = [];
     names = $('#namefield').val().split("\n");
@@ -32,10 +33,10 @@ function drawButtonPDF () {
     // http://stackoverflow.com/questions/281264/remove-empty-elements-from-an-array-in-javascript
     var len = names.length;
     for (var i = 0; i < len; i++ ) {
-	names[i] && names.push(names[i]);  // copy non-empty values to the end of the array
+        names[i] && names.push(names[i]);  // copy non-empty values to the end of the array
     }
     names.splice(0 , len);  // cut the array and leave only the non-empty values
-    
+
     var pages = 0;
 
     var buttons_per_page = maxrows * maxcols;
@@ -45,45 +46,44 @@ function drawButtonPDF () {
 
 
     while (names.length) {
-	doc.setLineWidth(.01);
-	for (var i=0; i < maxrows; i++) {
-	    var y = margintop + circle_radius +
-	    	(i * (circle_radius*2 + gutter) );
+        doc.setLineWidth(.01);
+        for (var i=0; i < maxrows; i++) {
+            var y = margintop + circle_radius +
+                    (i * (circle_radius*2 + gutter) );
 
-	    for (var j=0; j < maxcols; j++) {
-		var x = marginsides + circle_radius +
-		    (j * (circle_radius*2 + gutter) );
-	
-		if (i<4) {
-		    if (!logo) console.error("nothing in global `logo` var!");
+            for (var j=0; j < maxcols; j++) {
+                var x = marginsides + circle_radius +
+                    (j * (circle_radius*2 + gutter) );
 
-		    // scale up the image slightly to add a bleed 
+                if (i<4) {
+                    if (!logo) console.error("nothing in global `logo` var!");
+
+                    doc.addImage(logo, 'JPEG', x - img_radius, y - img_radius,
+                                 img_radius * 2, img_radius * 2);
+
+                    // Too bad, SVG support just not there yet (2015).
+//                doc.addSVG(logosvg,  x - circle_radius, y - circle_radius, 2.5, 2.5);
+                }
+
+// faint outline for cutting the circle?
+
+//                 doc.setDrawColor(200);
+//                 doc.setLineWidth(.001);
+//                 doc.circle(x, y, circle_radius);
+//
+// //                doc.circle(x, y, design_radius);
+//                 doc.setDrawColor(0);
 
 
-		    doc.addImage(logo, 'JPEG', x - img_radius, y - img_radius, 
-				 img_radius * 2, img_radius * 2); 
-
-		    // Too bad, SVG support just not there yet.
-//		doc.addSVG(logosvg,  x - circle_radius, y - circle_radius, 2.5, 2.5); 
-		}
-
-		doc.setDrawColor(200);
-		doc.setLineWidth(.001);
-		doc.circle(x, y, circle_radius);
-
-//		doc.circle(x, y, design_radius);
-		doc.setDrawColor(0);
-
-
-		var n = '';
-		if (n = names.shift()) {
-		    drawName(x, y - .1, n);
-		}
-	    }
-	}
-	if (names.length) {
-	    doc.addPage();
-	}
+                var n = '';
+                if (n = names.shift()) {
+                    drawName(x, y - .1, n);
+                }
+            }
+        }
+        if (names.length) {
+            doc.addPage();
+        }
     }
 
 
@@ -115,69 +115,69 @@ function drawName (centerx, centery, n) {
     var lines = [];
 
     while (!found_fit && found_size >= min_size) {
-    	// Try it on one line
-    	lines = [n];
-    	if (doc.getStringUnitWidth(lines[0]) * found_size <= max_middle_line_width_in_pts) {
-    	    found_fit = true;
-    	    console.log('Found ', n, ' fits in one line at ', found_size, ' pts');
-	    break;
-    	}
+            // Try it on one line
+            lines = [n];
+            if (doc.getStringUnitWidth(lines[0]) * found_size <= max_middle_line_width_in_pts) {
+                found_fit = true;
+                console.log('Found ', n, ' fits in one line at ', found_size, ' pts');
+            break;
+            }
 
-	var hyphenbroken = [];
-	// Try on two lines breaking on dashes
-	if (n.indexOf('-') > -1) {
-	    var hyphenated = n.split(/-/);
-	    lines[0] = hyphenated[0] + "-";
-	    lines[1] = hyphenated[1];
-	    hyphenbroken = lines;
-    	    if (doc.getStringUnitWidth(lines[0]) * found_size <= max_twolines_width &&
-		doc.getStringUnitWidth(lines[1]) * found_size <= max_twolines_width 
-	       ) {
-    		found_fit = true;
-    		// console.log('Found ', lines, ' fits in two lines at ', found_size, ' pts');
-		break;
-    	    }
-	}
-	else {
-    	    // Try it on two lines breaking on spaces.
-	    lines[0] = names.slice(0, names.length - 1).join(' ');
-	    lines[1] = names[names.length - 1];
-    	    if (doc.getStringUnitWidth(lines[0]) * found_size <= max_twolines_width &&
-		doc.getStringUnitWidth(lines[1]) * found_size <= max_twolines_width 
-	       ) {
-    		found_fit = true;
-    		// console.log('Found ', lines, ' fits in two lines at ', found_size, ' pts');
-		break;
-    	    }
+        var hyphenbroken = [];
+        // Try on two lines breaking on dashes
+        if (n.indexOf('-') > -1) {
+            var hyphenated = n.split(/-/);
+            lines[0] = hyphenated[0] + "-";
+            lines[1] = hyphenated[1];
+            hyphenbroken = lines;
+                if (doc.getStringUnitWidth(lines[0]) * found_size <= max_twolines_width &&
+                doc.getStringUnitWidth(lines[1]) * found_size <= max_twolines_width
+               ) {
+                    found_fit = true;
+                    // console.log('Found ', lines, ' fits in two lines at ', found_size, ' pts');
+                break;
+                }
+        }
+        else {
+                // Try it on two lines breaking on spaces.
+            lines[0] = names.slice(0, names.length - 1).join(' ');
+            lines[1] = names[names.length - 1];
+                if (doc.getStringUnitWidth(lines[0]) * found_size <= max_twolines_width &&
+                doc.getStringUnitWidth(lines[1]) * found_size <= max_twolines_width
+               ) {
+                    found_fit = true;
+                    // console.log('Found ', lines, ' fits in two lines at ', found_size, ' pts');
+                break;
+                }
 
-	}
+        }
 
 
-    	// Try three lines
-	if (found_size <= max_3line_size) {
+            // Try three lines
+        if (found_size <= max_3line_size) {
 
-	    if (hyphenbroken.length > 0) {
-		lines = hyphenbroken;
-	    }
-	    if (lines[0].indexOf(' ') > -1 ) {
-		lines[2] = lines[1];
-		var split_top_line = lines[0].split(/ /);
+            if (hyphenbroken.length > 0) {
+                lines = hyphenbroken;
+            }
+            if (lines[0].indexOf(' ') > -1 ) {
+                lines[2] = lines[1];
+                var split_top_line = lines[0].split(/ /);
 
-		lines[0] = split_top_line.slice(0, split_top_line.length - 1).join(' ');
-		lines[1] = split_top_line[split_top_line.length - 1];
-    		if (doc.getStringUnitWidth(lines[0]) * found_size <= max_thirdlines_width &&
-		    doc.getStringUnitWidth(lines[1]) * found_size <= max_middle_line_width_in_pts &&
-		    doc.getStringUnitWidth(lines[2]) * found_size <= max_thirdlines_width 
-		   ) {
-    		    found_fit = true;
-    		    // console.log('Found ', lines, ' fits in three lines at ', found_size, ' pts');
-		    break;
-    		}
-	    }
-	}
+                lines[0] = split_top_line.slice(0, split_top_line.length - 1).join(' ');
+                lines[1] = split_top_line[split_top_line.length - 1];
+                    if (doc.getStringUnitWidth(lines[0]) * found_size <= max_thirdlines_width &&
+                    doc.getStringUnitWidth(lines[1]) * found_size <= max_middle_line_width_in_pts &&
+                    doc.getStringUnitWidth(lines[2]) * found_size <= max_thirdlines_width
+                   ) {
+                        found_fit = true;
+                        // console.log('Found ', lines, ' fits in three lines at ', found_size, ' pts');
+                    break;
+                    }
+            }
+        }
 
-    	// Try making it smaller
-    	found_size--;
+            // Try making it smaller
+            found_size--;
     }
 
     var leading_pts = found_size * 0.2;
@@ -185,16 +185,16 @@ function drawName (centerx, centery, n) {
     var line_height_in = (found_size + leading_pts) / ppi;
 
     for (var i=0; i<lines.length; i++) {
-    	var xoffset = 0 - (doc.getStringUnitWidth(lines[i]) * found_size/ppi / 2);
+            var xoffset = 0 - (doc.getStringUnitWidth(lines[i]) * found_size/ppi / 2);
 
-	// .67 because the full height includes descenders which should drop below center.
-	var textheight = (lines.length * found_size + (lines.length - 1) * leading_pts)/ppi;
+        // .67 because the full height includes descenders which should drop below center.
+        var textheight = (lines.length * found_size + (lines.length - 1) * leading_pts)/ppi;
 
-//    	var yoffset = (line_height_in * .67) * lines.length/ 2 - (lines.length - 1 - i) * line_height_in;
-    	var yoffset = name_yoffset + (found_size/ppi * .6) - textheight / 2 + i * line_height_in;
-    	doc.setFontSize(found_size);
-    	// console.log('offsetting ', n, ' by ', xoffset, 'in from ', centerx);
-    	doc.text(centerx + xoffset, centery + yoffset, lines[i]);
+//            var yoffset = (line_height_in * .67) * lines.length/ 2 - (lines.length - 1 - i) * line_height_in;
+            var yoffset = name_yoffset + (found_size/ppi * .6) - textheight / 2 + i * line_height_in;
+            doc.setFontSize(found_size);
+            // console.log('offsetting ', n, ' by ', xoffset, 'in from ', centerx);
+            doc.text(centerx + xoffset, centery + yoffset, lines[i]);
     }
 
 }
@@ -203,37 +203,37 @@ function drawName (centerx, centery, n) {
 
 $(document).ready(function() {
 
-    round_button_cookie = $.cookie(cookie_name); 
+    round_button_cookie = $.cookie(cookie_name);
     if (round_button_cookie === undefined) {
-	$.cookie(cookie_name, $('#namefield').val(), { expires: 366*2 });
+        $.cookie(cookie_name, $('#namefield').val(), { expires: 366*2 });
     }
     else {
-	$('#namefield').val(round_button_cookie);
+        $('#namefield').val(round_button_cookie);
     }
 
 
     $('.download-pdf').click(function(){
-	console.log('hello download pdf click handler');
-	drawButtonPDF();
-	doc.save('name-buttons.pdf');
+        console.log('hello download pdf click handler');
+        drawButtonPDF();
+        doc.save('name-buttons.pdf');
     });
 
     $('#namefield').bind('input propertychange', function() {
-	console.log("change event");
-	setTimeout(
-	    function() {
-		console.log('rewriting cookie');
-		$.removeCookie(cookie_name);
-		$.cookie(cookie_name, $('#namefield').val(), { expires: 366*2 });
+        console.log("change event");
+        setTimeout(
+            function() {
+                console.log('rewriting cookie');
+                $.removeCookie(cookie_name);
+                $.cookie(cookie_name, $('#namefield').val(), { expires: 366*2 });
 
-		drawButtonPDF();
-		var PDFstring = doc.output('datauristring');
-		$('.preview-pane').attr('src', PDFstring);
-	    }
-	    ,0
-	);
+                drawButtonPDF();
+                var PDFstring = doc.output('datauristring');
+                $('.preview-pane').attr('src', PDFstring);
+            }
+            ,0
+        );
 
-	
+
     });
 
 
